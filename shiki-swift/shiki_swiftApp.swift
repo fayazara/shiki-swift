4ba55ae90294d088f5e8054f75c0bd74d093ffa0
Fayaz Ahmed <fayazara@gmail.com>
2026-08-14 15:40:45 +0530

    Initial Commit

---
+++ shiki-swift/shiki_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  shiki_swiftApp.swift
+//  shiki-swift
+//
+//  Created by Fayaz Ahmed Aralikatti on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct shiki_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
